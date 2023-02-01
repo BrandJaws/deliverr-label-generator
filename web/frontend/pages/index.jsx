7fc08ab1fce7038a11dbdfadf3f44f61c843d745
mohsinikramvu <mohsin.brandjaws@gmail.com>
2023-02-01 21:37:48 +0500

    initial commit

---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,146 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Heading,
+  Form,
+  FormLayout,
+  Checkbox,
+  TextField,
+  Button,
+  Spinner
+} from "@shopify/polaris";
+import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
+import { getSessionToken, authenticatedFetch } from "@shopify/app-bridge-utils";
+
+import { useState, useCallback } from "react";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+import { json } from "react-router-dom";
+
+export default function HomePage() {
+  const app = useAppBridge();
+  const [value, setValue] = useState("30");
+  const [isResult, setIsResult] = useState(false);
+  const [authenticatedCode, setAuthencatedCode] = useState("");
+  const [isResponseOk, setResponseOk] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [responseText, setResponseText] = useState("");
+  
+  const handleChange = useCallback((newValue) => setValue(newValue), []);
+
+  const handleSubmit = useCallback((_event) => {
+    window.open(
+      "https://api.deliverr.com/oauth/v1/authorize?scopes=api/parcel-integration&redirect_uri=https://culturefly.myshopify.com/admin/apps/deliverr-custom-app",
+      "_blank"
+    );
+  }, []);
+
+  const spinFunction = () => {
+    return <Spinner accessibilityLabel="Spinner example" size="large" />;
+  }
+
+  window.onload = async function () {
+    let urlString = window.location.search;
+    const urlParams = new URLSearchParams(urlString);
+    let code = urlParams.get("code");
+    // console.log(code);
+
+    if (code != null) {
+      setAuthencatedCode(code);
+      setIsResult(true);
+    } else {
+      // console.log("code is null");
+      setAuthencatedCode("");
+      setIsResult(false);
+    }
+  };
+  
+  async function getFinalResponse(token, access_token, value){
+    // console.log("Dummy Function called", token);
+    const responseDummy = await fetch('/api/labelgenerator', {
+      method: "POST",
+      headers: { 
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+       },
+      body: JSON.stringify({'access_token':access_token, 'value':value})
+    });
+    // console.log(responseDummy);
+    const data = await responseDummy.text();
+    console.log(data);
+    if(data){
+      setResponseOk(true);
+      setIsResult(false);
+      setResponseText(data);
+      setIsLoading(false);
+    }
+    else
+    {
+       setResponseOk(false); 
+    }
+  }
+  const handleApi = async function () {
+    setIsResult(false);
+    const token = await getSessionToken(app);
+
+    setIsLoading(true)
+    // console.log("API CODE IS HERE: ", authenticatedCode, window.Shopify);
+    const token_response = await fetch(
+      "https://api.deliverr.com/oauth/v1/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: "code=" + authenticatedCode + "&grant_type=authorization_code",
+      }
+    );
+
+    const token_accessCode = await token_response.json();
+    console.log(token_accessCode.access_token);
+    if (token_accessCode != null){
+      getFinalResponse(token , token_accessCode.access_token, value);
+    }
+    
+    
+  };
+  return (
+    <Page narrowWidth>
+      <TitleBar title="Deliverr Label Generator" primaryAction={null} />
+      <Layout>
+        <Layout.Section>
+          {isResult ? (
+            <Form onSubmit={handleApi}>
+              <FormLayout>
+                <TextField
+                  type="number"
+                  label="Number of Order Count"
+                  autoComplete="off"
+                  value={value}
+                  onChange={handleChange}
+                ></TextField>
+                <Button submit onClick={spinFunction}>Submit</Button>
+              </FormLayout>
+            </Form>
+          ) : !isResponseOk && !isLoading && (
+            <Button onClick={handleSubmit}>Generate Labels</Button>
+          ) }
+          {isResponseOk && <p>{responseText}</p>}
+          {isLoading && (
+            <div>
+              <h1>Loading...</h1>
+              <Spinner accessibilityLabel="Spinner example" size="large" />
+            </div>
+          )}
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
---
+++ web/index.js
@@ -0,0 +1,94 @@
+// @ts-check
+import { join } from "path";
+import { readFileSync } from "fs";
+import express from "express";
+import serveStatic from "serve-static";
+import fetch from 'node-fetch'
+
+import shopify from "./shopify.js";
+import productCreator from "./product-creator.js";
+import GDPRWebhookHandlers from "./gdpr.js";
+
+const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
+
+const STATIC_PATH =
+  process.env.NODE_ENV === "production"
+    ? `${process.cwd()}/frontend/dist`
+    : `${process.cwd()}/frontend/`;
+
+const app = express();
+
+// Set up Shopify authentication and webhook handling
+app.get(shopify.config.auth.path, shopify.auth.begin());
+app.get(
+  shopify.config.auth.callbackPath,
+  shopify.auth.callback(),
+  shopify.redirectToShopifyOrAppRoot()
+);
+app.post(
+  shopify.config.webhooks.path,
+  shopify.processWebhooks({ webhookHandlers: GDPRWebhookHandlers })
+);
+
+// All endpoints after this point will require an active session
+app.use("/api/*", shopify.validateAuthenticatedSession());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/products/count", async (_req, res) => {
+  const countData = await shopify.api.rest.Product.count({
+    session: res.locals.shopify.session,
+  });
+  res.status(200).send(countData);
+});
+
+app.get("/api/products/create", async (_req, res) => {
+  let status = 200;
+  let error = null;
+
+  try {
+    await productCreator(res.locals.shopify.session);
+  } catch (e) {
+    console.log(`Failed to process products/create: ${e.message}`);
+    status = 500;
+    error = e.message;
+  }
+  res.status(status).send({ success: status === 200, error });
+});
+
+app.use(serveStatic(STATIC_PATH, { index: false }));
+
+
+app.post('/api/labelgenerator', async (_req, res) => {
+  const job_response = await fetch('https://api.deliverr.com/parcel-integration/v1/job', {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer " + _req.body.access_token,
+    },
+    body:JSON.stringify({
+      'integrationChannelId':'shipstation',
+      'estimatedOrderCount': _req.body.value
+    }) 
+    //'{"integrationChannelId":"shipstation","estimatedOrderCount": _req.body.value}' 
+    //'integrationChannelId=shipstation&estimatedOrderCount='+_req.body.value
+  });
+  //console.log(job_response);
+  const final_response = await job_response.text();
+  console.log(final_response);
+    
+    
+  res.status(200).send(final_response)
+});
+
+app.use("/*", shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
+  return res
+    .status(200)
+    .set("Content-Type", "text/html")
+    .send(readFileSync(join(STATIC_PATH, "index.html")));
+});
+
+
+
+app.listen(PORT);
